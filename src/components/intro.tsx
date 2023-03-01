a0f02fcc1f3d7b01d173f19496beeb1d24b13306
Bowie W <bowiewong12@gmail.com>
2023-02-28 20:54:42 -0800

    first letter animation switching finished

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,35 +4,62 @@ export default function intro() {
 
     return (
         <div className="intro_main">
-            <h1 className="intro_title_box">
-                <span className="intro_title_box_letter">
-                    M
-                </span>
-                <span className="intro_title_box_letter">
-                    A
-                </span>
-                <span className="intro_title_box_letter">
-                    D
-                </span>
-                <span className="intro_title_box_letter">
-                    E
-                </span>
-                <span className="intro_title_box_letter">
-                    L
-                </span>
-                <span className="intro_title_box_letter">
-                    U
-                </span>
-                <span className="intro_title_box_letter">
-                    S
-                </span>
-                <span className="intro_title_box_letter">
-                    I
-                </span>
-                <span className="intro_title_box_letter">
-                    V
-                </span>
-            </h1>
+            <div className="intro_title_box">
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        M
+                    </span>
+                    <span className="intro_title_box_letter_alternate">
+                        S
+                    </span>
+                    <span className="intro_title_box_letter_alternate">
+                        T
+                    </span>
+                    <span className="intro_title_box_letter_alternate">
+                        B
+                    </span>
+                </div>
+
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        A
+                    </span>
+                </div>
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        D
+                    </span>
+                </div>  <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        E
+                    </span>
+                </div>
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        L
+                    </span>
+                </div>
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        U
+                    </span>
+                </div>
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        S
+                    </span>
+                </div>
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        I
+                    </span>
+                </div>
+                <div className='intro_title_box_Box'>
+                    <span className="intro_title_box_letter">
+                        V
+                    </span>
+                </div>
+            </div>
 
 
 
