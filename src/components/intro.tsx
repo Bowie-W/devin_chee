efb72a6dab3d7d356cdd7e46d3294d95540dd3ce
Bowie W <bowiewong12@gmail.com>
2023-02-28 19:41:04 -0800

    first, intro screen base laid out

---
+++ src/components/intro.tsx
@@ -0,0 +1,42 @@
+import './Intro.scss'
+
+export default function intro() {
+
+    return (
+        <div className="intro_main">
+            <h1 className="intro_title_box">
+                <span className="intro_title_box_letter">
+                    M
+                </span>
+                <span className="intro_title_box_letter">
+                    A
+                </span>
+                <span className="intro_title_box_letter">
+                    D
+                </span>
+                <span className="intro_title_box_letter">
+                    E
+                </span>
+                <span className="intro_title_box_letter">
+                    L
+                </span>
+                <span className="intro_title_box_letter">
+                    U
+                </span>
+                <span className="intro_title_box_letter">
+                    S
+                </span>
+                <span className="intro_title_box_letter">
+                    I
+                </span>
+                <span className="intro_title_box_letter">
+                    V
+                </span>
+            </h1>
+
+
+
+        </div>
+
+    )
+}
