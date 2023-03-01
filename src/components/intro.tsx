d90eb5b03b29857ec5973a5581c7989db5dabdfe
Bowie W <bowiewong12@gmail.com>
2023-02-28 21:23:20 -0800

    first letter animation replaced span elements with map function

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -2,27 +2,36 @@ import './Intro.scss'
 
 export default function intro() {
 
+
+    const intro_alt_1 = ['S', 'T', 'B', 'R', 'E', 'N']
+
     return (
         <div className="intro_main">
             <div className="intro_title_box">
                 <div className='intro_title_box_Box'>
-                    <span className="intro_title_box_letter">
+                    <span className="intro_title_box_letter1">
                         M
                     </span>
-                    <span className="intro_title_box_letter_alternate">
-                        S
-                    </span>
-                    <span className="intro_title_box_letter_alternate">
-                        T
-                    </span>
-                    <span className="intro_title_box_letter_alternate">
-                        B
-                    </span>
+                    {intro_alt_1.map((letter) => (
+                        <span className="intro_title_box_letter_alternate">
+                            {letter}
+                        </span>
+                    ))}
+
                 </div>
 
                 <div className='intro_title_box_Box'>
                     <span className="intro_title_box_letter">
                         A
+                    </span>
+                    <span className="intro_title_box_letter_alternate2">
+                        M
+                    </span>
+                    <span className="intro_title_box_letter_alternate2">
+                        J
+                    </span>
+                    <span className="intro_title_box_letter_alternate2">
+                        P
                     </span>
                 </div>
                 <div className='intro_title_box_Box'>
