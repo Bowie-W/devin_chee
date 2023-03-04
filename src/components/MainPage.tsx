097725407a02b7067d8c837efadb5d8a83e64342
BW <bowiewong12@gmail.com>
2023-03-03 16:15:34 -0800

    added fade-ins, basic layout of site setup

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,16 +1,34 @@
 import './MainPage.scss'
+import Player from "@madzadev/audio-player";
+import "@madzadev/audio-player/dist/index.css";
 
 export default function MainPage() : JSX.Element{
 
+    const tracks = [
+        {
+            url :'https://res.cloudinary.com/dl2liojkl/video/upload/v1671132047/xg3vk8jhm72n4ttqslv2.mp3',
+            title : 'Pink and White',
+            tags: ["r&b"],
+        },
+        {
+            url :'https://res.cloudinary.com/dl2liojkl/video/upload/v1671132047/xg3vk8jhm72n4ttqslv2.mp3',
+            title : 'Pink and White',
+            tags: ["r&b", "rap"],
+        }
+
+    ]
+
     return(
         <div className='mainPage_container'>
-            <img className='mainPage_profilePic'></img>
+            <img className='mainPage_profilePic' src='https://res.cloudinary.com/dl2liojkl/image/upload/v1677886778/devin_chee-pic_imjsb5.jpg'></img>
             <div className='mainPage_bio_box'>
-                <h2 className='maingPage_bio_box_tag'></h2>
-                <p className='mainPage_bio_box_mainText'></p>
+                <h2 className='mainPage_bio_box_tag'>de_vinchee</h2>
+                <p className='mainPage_bio_box_mainText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div className='mainPage_audio'>
-                <div className='mainPage_audio_musicPlayer'></div>
+                <div className='mainPage_audio_musicPlayer'>
+                    <Player trackList={tracks}/>
+                </div>
                 <div className='mainPage_audio_tracks'> </div>
             </div>
 
