1343be2fdaad6f23db57870e94fb594cf8c996e9
BW <bowiewong12@gmail.com>
2023-03-18 15:43:31 -0700

    add audio player component basic

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,13 +1,36 @@
 import './LandingPage.scss'
 import Header from './Header'
 import MainPage from './MainPage'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function LandingPage() : JSX.Element{
+
+    const [profile, setProfile] = useState({})
+
+    useEffect(()=> {
+        axios
+        .get("http://localhost:3030/tracks")
+        .then((response)=>{
+            console.log(response)
+        })    
+
+    }, [])
+
+    useEffect(()=> {
+        axios
+        .get("http://localhost:3030/profile")
+        .then((response) => {
+            setProfile(response.data[0])
+        })
+
+    }, [])
+
 
     return (
         <div className='landingPage_main'>
             <Header/>
-            <MainPage/>
+            <MainPage profile={profile}/>
 
         </div>
 
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,10 @@
 import './MainPage.scss'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { Audioplayer } from './Audioplayer'
 
-export default function MainPage() : JSX.Element{
+export default function MainPage({profile}) : JSX.Element{
+
 
     const tracks = [
         {
@@ -14,18 +18,20 @@ export default function MainPage() : JSX.Element{
             tags: ["r&b", "rap"],
         }
 
+        
     ]
+
+    console.log(profile.picture)
 
     return(
         <div className='mainPage_container'>
-            <img className='mainPage_profilePic' src='https://res.cloudinary.com/dl2liojkl/image/upload/v1677886778/devin_chee-pic_imjsb5.jpg'></img>
+            <img className='mainPage_profilePic' src={profile.picture}></img>
             <div className='mainPage_bio_box'>
                 <h2 className='mainPage_bio_box_tag'>de_vinchee</h2>
                 <p className='mainPage_bio_box_mainText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div className='mainPage_audio'>
-                <div className='mainPage_audio_musicPlayer'>
-                </div>
+                <Audioplayer/>
                 <div className='mainPage_audio_tracks'> </div>
             </div>
 
