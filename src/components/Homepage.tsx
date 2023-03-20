388812f168e7045b083be8f2f2dcad6e97b213b9
BW <bowiewong12@gmail.com>
2023-03-20 16:27:02 -0700

    have info loading during intro animation, with animations timed

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,47 +1,51 @@
-import axios from 'axios'
-import { ReactComponentElement, ReactElement, useEffect, useState } from 'react'
-import './Homepage.scss'
-import Intro from './intro'
-import LandingPage from './LandingPage'
-
+import axios from "axios";
+import {
+  ReactComponentElement,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
+import "./Homepage.scss";
+import Intro from "./intro";
+import LandingPage from "./LandingPage";
 
 function Homepage() {
+  const [introEle, setIntroEle] = useState<JSX.Element | null>(<Intro />);
+  const [landPageStatus, setLandPageStatus] = useState<JSX.Element | null>(
+    null
+  );
+  const [pageDisplay, setPageDisplay] = useState("homepage_landingpage-hidden")
 
-  const [introEle, setIntroEle] = useState< JSX.Element| null>(<Intro/>)
-  const [landPageStatus, setLandPageStatus] = useState <JSX.Element | null>(null)
+  const [profile, setProfile] = useState({});
+  const [tracks, setTracks] = useState([]);
 
-  const [profile, setProfile] = useState({})
-  const [tracks, setTracks] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:3030/tracks").then((response) => {
+      setTracks(response.data);
+    });
+  }, []);
 
-    useEffect(()=> {
-        axios
-        .get("http://localhost:3030/tracks")
-        .then((response)=>{
-            setTracks(response.data)
-        })    
+  useEffect(() => {
+    axios.get("http://localhost:3030/profile").then((response) => {
+      setProfile(response.data[0]);
+    });
+  }, []);
 
-    }, [])
-
-    useEffect(()=> {
-        axios
-        .get("http://localhost:3030/profile")
-        .then((response) => {
-            setProfile(response.data[0])
-        })
-
-    }, [])
-
-  setTimeout(()=>{
-    setIntroEle(null)
-    setLandPageStatus(<LandingPage profile={profile} tracks={tracks}/>)
-}, 6250)
+  setTimeout(() => {
+    setIntroEle(null);
+    setPageDisplay("homepage_landingpage")
+    
+  }, 6250);
 
   return (
-    <div className="Homepage">
-     {introEle} 
-     {landPageStatus}
+    <div className="homepage">
+      <div className="homepage_intro">{introEle}</div>
+      <div className={pageDisplay}>
+        <LandingPage profile={profile} tracks={tracks} />
+      </div>
+   
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
