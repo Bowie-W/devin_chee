8241a7c957c77b946184da92726696328e597370
BW <bowiewong12@gmail.com>
2023-04-04 13:19:33 -0700

    add basic format for EP component

--- src/components/EPs.tsx
+++ src/components/EPs.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import './EPs.scss'
+import React from "react";
+import "./EPs.scss";
 
-function EPs({eps}) {
+function EPs({ eps }) {
+
+  const playTrack = (event) => {
+    console.log(event.target.attributes.value.value)
+  }
   return (
-    <div className='EPs_container'>
-
-        EPs
+    <div className="EPs_container">
+      <div className="EPs_innerContainer">
+        {eps.map((ep) => (
+          <div className="singleEp_container">
+            <div className="singleEp_leftside">
+              <img className="singleEp_cover" src={ep.cover} />
+              <h1 className="singleEp_title">{ep.title}</h1>
+            </div>
+            <div className="singleEp_rightside">
+              {ep.tracks.map((track) => (
+                <div className="singleEp_rightside-trackBox" onClick={playTrack} value={track.trackUrl}>
+                  {track.trackTitle}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default EPs
+export default EPs;
